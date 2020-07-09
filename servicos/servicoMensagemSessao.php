fa7fe3d21fe530e8fadb7edbe445e55050c11ffd
Joao Erick <jerick1415@outlook.com>
2020-07-09 13:29:23 -0300

    Criando funções para as mensagens de sessão

--- servicos/servicoMensagemSessao.php
+++ servicos/servicoMensagemSessao.php
@@ -12,3 +12,26 @@ function obterMensagemErro() : ?string {
     }
     return null;
 }
+
+
+function setarMensagemSucesso(string $mensagem) : void {
+    $_SESSION['mensagem-de-sucesso'] = $mensagem;
+}
+
+function obterMensagemSucesso() : ?string {
+    if(isset($_SESSION['mensagem-de-sucesso'])){
+        return $_SESSION['mensagem-de-sucesso'];
+    }
+    return null;
+}
+
+
+function removerMensagemErro() : void{
+    if(isset($_SESSION['mensagem-de-erro']))
+        unset($_SESSION['mensagem-de-erro']);
+}
+function removerMensagemSucesso() : void{
+    if(isset($_SESSION['mensagem-de-sucesso']))
+        unset($_SESSION['mensagem-de-sucesso']);
+
+}
